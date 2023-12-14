1d347edc284ba371fd2021818542d9fae8e53480
byronwai <byronwai@gmail.com>
2023-12-14 13:33:24 +0800

    Create compare2.py

---
+++ 2023-24/Lesson07/ #!/usr/bin/compare2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+a = int(input('Please enter number a: '))
+b = int(input('Please enter number b: '))
+
+if (a > b):
+  print('a is larger than b')
+else:
+  print('a is not larger than b')
